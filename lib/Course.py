5c4b3cf3eafdc6700b900c5cf8e3511682870b7b
akamnovelakwa-ship-it <akam.akwa@student.moringaschool.com>
2026-01-06 22:55:12 +0300

    lab

--- lib/Course.py
+++ lib/Course.py
@@ -1,2 +1,14 @@
+
 class Course:
-    pass
+    def __init__(self, title=None, schedule=None, description=None):
+        self.title = title
+        self.schedule = schedule
+        self.description = description
+
+    def __str__(self):
+        output = ""
+        output += f"Title: {self.title}\n"
+        output += f"Schedule: {self.schedule}\n"
+        output += f"Description: {self.description}\n"
+        output += "------------------"
+        return output
